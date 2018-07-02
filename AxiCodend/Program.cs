1857ed28f67ba458e16bc8fded38e099e83cfd13
Mihhail Samusev <msamus16@student.aau.dk>
2018-07-02 13:20:05 +0200

    final

--- AxiCodend/Program.cs
+++ AxiCodend/Program.cs
@@ -17,7 +17,7 @@ namespace AxiCodend
 
             try
             {
-                Console.WindowWidth = 160;
+                Console.WindowWidth = 100;
                 Console.WindowHeight = 50;
             }
             catch
